e5addb7f181b62be4fcdf85811275f9e433139a7
bryan <bryantift@rocketmail.com>
2017-02-27 18:08:04 -0800

    Added tring

--- src/main/java/com/dauntless/commons/bukkit/cooldown/Cooldown.java
+++ src/main/java/com/dauntless/commons/bukkit/cooldown/Cooldown.java
@@ -4,4 +4,9 @@ package com.dauntless.commons.bukkit.cooldown;
  * Created by GamerzKing on 2/27/2017
  */
 public class Cooldown {
+
+    public Cooldown() {
+        String s = "It's a me, cooldown!";
+    }
+
 }
